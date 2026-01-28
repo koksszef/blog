e4cdad6e756bff263c433145f50bff61d95ffbcd
koksszef <146476154+koksszef@users.noreply.github.com>
2026-01-28 17:52:18 +0100

    Update header.js

--- js/header.js
+++ js/header.js
@@ -3,12 +3,11 @@ const header = document.createElement('header');
 header.innerHTML = `
 <nav>
 <ul>
-<li><a href="/" class="nav-link">Start</a></li>
-<li><a href="/home" class="nav-link">Home</a></li>
-<li><a href="/short" class="nav-link">Broń krótka</a></li>
-<li><a href="/long" class="nav-link">Broń długa</a></li>
-<li><a href="/other" class="nav-link">Inne</a></li>
-<li><a href="/about" class="nav-link">About us</a></li>
+<li><a href="blog" class="nav-link">BLOG</a></li>
+<li><a href="bron-krotka" class="nav-link">Broń krótka</a></li>
+<li><a href="bron-dluga" class="nav-link">Broń długa</a></li>
+<li><a href="inne" class="nav-link">Inne</a></li>
+<li><a href="o-stronie" class="nav-link">O Stronie</a></li>
 </ul>
 </nav>
 `;
@@ -21,4 +20,5 @@ links.forEach(link => {
 if (link.href === window.location.href || link.href === window.location.pathname) {
 link.classList.add('active');
 }
+
 });
